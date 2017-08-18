e63ea6765121b6ecd036b134bb5c519f687cb4e7
Alexander Campbell <acampbell2231@gmail.com>
2017-08-18 14:00:28 -0600

    Mammal Class

---
+++ mammal.rb
@@ -0,0 +1,18 @@
+class Mammal #classes always start with CAPITAL
+  attr_accessor :name, :age
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+
+  def breathe
+    puts "inhale and exhale"
+  end
+
+  def speak
+    raise "you must override this in a child class"
+  end
+end
+
+Mammal.new
